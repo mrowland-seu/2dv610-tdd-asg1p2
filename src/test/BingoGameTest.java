bdf0fb180deee541d837e62da3b9188bf2ab4157
mrowland-seu <mrowland-seu@users.noreply.github.com>
2018-11-30 05:58:17 +0100

    red (new test: constructorCompletedSuccessfullyForRowBasedBingoCard())

--- src/test/BingoGameTest.java
+++ src/test/BingoGameTest.java
@@ -17,5 +17,22 @@ public class BingoGameTest {
     public void initTests() {
     }
 
+    @Test
+    public void constructorCompletedSuccessfullyForRowBasedBingoCard() {
+        BingoCard bingoCard = createRowListBingoCard(getLowestPossibleBingoCardValueArray());
+    }
 
+    private BingoCard createRowListBingoCard(Integer[][] values) {
+        return new BingoCardImpl_RowList(values);
+    }
+
+    private Integer[][] getLowestPossibleBingoCardValueArray() {
+        return new Integer[][] {
+                {1,16,31,46,61},
+                {2,17,31,47,62},
+                {3,18,BingoCard.FREE_SPACE,48,63},
+                {4,19,33,48,64},
+                {5,20,35,50,65}
+        };
+    }
 }
